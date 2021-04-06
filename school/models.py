e45260a4e309a89ed0b8248aaf5807d389b1271e
Александр Смолин <smolll123@mail.ru>
2021-04-06 20:52:00 +0300

    Created Student model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    second_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    family_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=40)
+    birthday = models.DateField()
+    #class_id = models.ForeignKey(Class)
+
 # Create your models here.
