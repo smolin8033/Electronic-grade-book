dc28531c4d31ae12e08fe600ed691f8d472d5441
Александр Смолин <smolll123@mail.ru>
2021-04-06 19:15:58 +0300

    Change database DATABASES to a new one, working with mySQL

--- electronicgrade/electronicgrade/settings.py
+++ electronicgrade/electronicgrade/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'electronicgrade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'electronicgrade',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'kjsvbftm',
     }
 }
 
