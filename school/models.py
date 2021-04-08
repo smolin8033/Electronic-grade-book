a11b5bbfc437c2dfdd606202f7e4d7981bf6db1f
Александр Смолин <smolll123@mail.ru>
2021-04-08 22:13:36 +0300

    Added class Class, migrations made and migrated

--- school/models.py
+++ school/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
+class Class(models.Model):
+    group = models.CharField(max_length=30)
+    class_number = models.IntegerField()
+    # type_of_class = models.ForeignKey(TypeOfClass)
+
+
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     family_name = models.CharField(max_length=50)
     address = models.CharField(max_length=40)
     birthday = models.DateField()
-    # class_id = models.ForeignKey(Class)
+    class_id = models.ForeignKey(Class, on_delete= models.DO_NOTHING, default="")
 
     def get_full_name(self):
         return "%s %s %s" % (self.first_name, self.second_name, self.family_name)
@@ -15,4 +21,5 @@ class Student(models.Model):
     def __str__(self):
         return "%s %s %s, address: %s " % \
                (self.first_name, self.second_name, self.family_name, self.address)
+
 # Create your models here.
