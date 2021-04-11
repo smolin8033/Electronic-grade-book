7a66dec3a02afbe034bb826a356a73a42e28bf4f
Александр Смолин <smolll123@mail.ru>
2021-04-11 21:44:57 +0300

    import Discipline

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Class, TypeOfClass, Teacher
+from .models import Student, Class, TypeOfClass, Teacher, Discipline
 
 
 class StudentAdmin(admin.ModelAdmin):
