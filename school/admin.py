c592929e9ced6a989dd9c195340acc4ab692f6e3
Александр Смолин <smolll123@mail.ru>
2021-04-11 20:07:39 +0300

    create ClassAdmin

--- school/admin.py
+++ school/admin.py
@@ -6,6 +6,10 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ("first_name", "second_name", "family_name", "birthday", "address")
 
 
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("group", "class_number")
+
+
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Class)
 admin.site.register(TypeOfClass)
