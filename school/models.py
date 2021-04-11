cdaa077b91740981ec409253056ec96f00701330
Александр Смолин <smolll123@mail.ru>
2021-04-11 21:55:24 +0300

    add __str__ method to class Teacher

--- school/models.py
+++ school/models.py
@@ -43,6 +43,10 @@ class Teacher(models.Model):
     salary = models.CharField(max_length=50)  # add money form
     gender = models.CharField(max_length=20)
 
+    def __str__(self):
+        return "%s %s %s" % \
+               (self.family_name, self.first_name, self.second_name)
+
 
 class Discipline(models.Model):
     name = models.CharField(max_length=40)
