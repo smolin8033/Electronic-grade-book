9deb6fa6ac90ddf6f0ed84b443e0991911d993b6
Александр Смолин <smolll123@mail.ru>
2021-07-10 19:22:49 +0300

    added student_all view to path

--- school/urls.py
+++ school/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.test_view),
     path('student/current/', views.student_current),
-    path('student/all/', views.student_current),
+    path('student/all/', views.student_all),
 ]
