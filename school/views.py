be6b00292b69c40246cdd173e42e46f4ddcfe39c
Александр Смолин <smolll123@mail.ru>
2021-07-10 19:27:54 +0300

    edited student_all function. Now it shows all tasks and redirects to the current ones

--- school/views.py
+++ school/views.py
@@ -22,4 +22,16 @@ def student_current(request):
     return render(request, "student_current.html", context)
 
 def student_all(request):
-    return render(request, "student_all.html", {})
+    student = Student.objects.get(id=4)
+    tasks_queryset = Task.objects.order_by('end_date')
+    marks_queryset = Mark.objects.all()
+    current_date = datetime.datetime.now()
+    if "btnform2" in request.POST:
+        return redirect("/school/student/current/")
+    context = {
+        "student": student,
+        "current_date": current_date,
+        "tasks_queryset": tasks_queryset,
+        "marks_queryset": marks_queryset,
+    }
+    return render(request, "student_all.html", context)
