79bedcd0989618d5b3999444df93337e3509c4ed
Александр Смолин <smolll123@mail.ru>
2021-07-10 18:52:33 +0300

    change student_view to student_current

--- school/urls.py
+++ school/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.test_view),
-    path('student/current/', views.student_view),
-    path('student/all/', views.student_view),
+    path('student/current/', views.student_current),
+    path('student/all/', views.student_current),
 ]
