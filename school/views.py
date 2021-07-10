0bda25bedd83915a0c6afee182738e33ae2c1d0c
Александр Смолин <smolll123@mail.ru>
2021-07-10 16:25:21 +0300

    Changing the variables names

--- school/views.py
+++ school/views.py
@@ -7,14 +7,14 @@ def test_view(request):
     return render(request, "login.html")
 
 def student_view(request):
-    st = Student.objects.get(id=4)
-    ta_queryset = Task.objects.order_by('end_date')[:10]
-    ma_queryset = Mark.objects.all()
+    student = Student.objects.get(id=4)
+    tasks_queryset = Task.objects.order_by('end_date')[:10]
+    marks_queryset = Mark.objects.all()
     current_date = datetime.datetime.now()
     context = {
-        "st": st,
-        "cd": current_date,
-        "ta": ta_queryset,
-        "ma": ma_queryset,
+        "student": student,
+        "current_date": current_date,
+        "tasks_queryset": tasks_queryset,
+        "marks_queryset": marks_queryset,
     }
     return render(request, "student.html", context)
