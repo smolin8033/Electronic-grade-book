0af3c9e2078949bf09a664bfb51d75095ae22666
Александр Смолин <smolll123@mail.ru>
2021-07-10 18:51:51 +0300

    change student_view to student_current

--- school/views.py
+++ school/views.py
@@ -6,13 +6,13 @@ from .models import Student, Class, Task, Mark
 def test_view(request):
     return render(request, "login.html")
 
-def student_view(request):
+def student_current(request):
     student = Student.objects.get(id=4)
     tasks_queryset = Task.objects.order_by('end_date')[:10]
     marks_queryset = Mark.objects.all()
     current_date = datetime.datetime.now()
     if "btnform1" in request.POST:
-        return redirect("/school/")
+        return redirect("/school/student/all/")
     context = {
         "student": student,
         "current_date": current_date,
