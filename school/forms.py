4a6d7ca83d633e9295d55fe0084850906a94d545
Александр Смолин <smolll123@mail.ru>
2021-08-08 20:43:36 +0300

    created MarkForm

--- school/forms.py
+++ school/forms.py
@@ -1,2 +1,13 @@
 from django import forms
 from .models import Mark
+
+class MarkForm(forms.ModelForm):
+    final_score = forms.IntegerField(label='', attrs={
+        "placeholder": "Mark given",
+    }
+    )
+    class Meta:
+        model = Mark
+        fields = [
+            "final_score",
+        ]
