4d8357da11cedc7cc4ace0614bf2417e131ee89b
Александр Смолин <smolll123@mail.ru>
2021-08-09 19:39:07 +0300

    add fields with Foreign keys to exclude

--- school/forms.py
+++ school/forms.py
@@ -4,12 +4,13 @@ from .models import Mark
 class MarkForm(forms.ModelForm):
     final_score = forms.IntegerField(label='', widget=forms.Textarea(
         attrs={
-            'placeholder': "Given mark",
+            "placeholder": "Given mark",
         }
     )
     )
     class Meta:
         model = Mark
+        exclude = ("student_id", "task_id")
         fields = [
             "final_score",
         ]
