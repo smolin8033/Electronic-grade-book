ae637e512db8d9e89c210a6f8b002e99f984bde7
Александр Смолин <smolll123@mail.ru>
2021-08-09 19:58:10 +0300

    delete widget from the form

--- school/forms.py
+++ school/forms.py
@@ -2,12 +2,8 @@ from django import forms
 from .models import Mark
 
 class MarkForm(forms.ModelForm):
-    final_score = forms.IntegerField(label='', widget=forms.Textarea(
-        attrs={
-            "placeholder": "Given mark",
-        }
-    )
-    )
+    final_score = forms.IntegerField(label='')
+
     class Meta:
         model = Mark
         exclude = ("student_id", "task_id")
