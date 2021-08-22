57ad0983f5b6fca418ae50f28c98f9fc76999a70
Александр Смолин <smolll123@mail.ru>
2021-08-22 23:05:40 +0300

    add teacher_unrated view to path

--- school/urls.py
+++ school/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('teacher/', views.teacher_interface, name='teacher_interface'),
     path('teacher/<int:pk>/class/', views.class_students, name="class_students"),
     path('teacher/<int:pk>/rated/', views.teacher_rated, name="teacher_rated"),
+    path('teacher/<int:pk>/unrated/', views.teacher_unrated, name="teacher_unrated"),
     path('teacher/<int:pk>/<int:rel_task>/mark/add/', views.mark_create_view, name="mark_add"),
     path('teacher/<int:pk>/mark/update/', views.MarkUpdateView.as_view(), name="mark_update"),
     path('teacher/<int:pk>/mark/delete/', views.MarkDeleteView.as_view(), name="mark_delete"),
