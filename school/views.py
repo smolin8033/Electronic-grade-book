6605a90d1538bf4a6628f96cabda9632aad34f18
Александр Смолин <smolll123@mail.ru>
2021-08-24 20:23:48 +0300

    redirect to "teacher_all_rated" page

--- school/views.py
+++ school/views.py
@@ -67,6 +67,8 @@ def teacher_rated(request, pk):
     current_date = datetime.datetime.now()
     if "to_unrated_tasks" in request.POST:
         return redirect("teacher_unrated", pk=student.id)
+    elif "to_all_tasks" in request.POST:
+        return redirect("teacher_all_rated", pk=student.id)
     context = {
         "student": student,
         "current_date": current_date,
