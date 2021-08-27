00398abf4c421967db0c8f0e4a66aa24aedacb24
Александр Смолин <smolll123@mail.ru>
2021-08-27 19:57:08 +0300

    add TaskCreateView to path

--- school/urls.py
+++ school/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('teacher/<int:pk>/mark/update/', views.MarkUpdateView.as_view(), name="mark_update"),
     path('teacher/<int:pk>/mark/delete/', views.MarkDeleteView.as_view(), name="mark_delete"),
     path('teacher/<int:pk>/tasks/', views.TaskListView.as_view(), name="teacher_tasks"),
+    path('teacher/<int:pk>/tasks/create/', views.TaskCreateView.as_view(), name="tasks_create"),
 ]
