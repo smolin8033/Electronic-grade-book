565e583f2a25c7d675cff2c99377bc1c44903e66
Александр Смолин <smolll123@mail.ru>
2021-08-27 18:06:16 +0300

    view now redirects to different pages (if user presses different buttons)

--- school/views.py
+++ school/views.py
@@ -42,12 +42,14 @@ def student_all(request):
 
 def teacher_interface(request):
     class_queryset = Class.objects.order_by("class_number", "group")
+    pk = request.POST.get("input1")
+    if request.method == "POST" and "students" in request.POST:
+        return redirect("class_students", pk=pk)
+    else:
+        return redirect("teacher_tasks", pk=pk)
     context = {
         "class_queryset": class_queryset,
     }
-    if request.method == "POST":
-        pk = request.POST.get("input1")
-        return redirect("class_students", pk=pk)
     return render(request, "teacher_interface.html", context)
 
 def class_students(request, pk):
