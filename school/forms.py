2b39fca59763c57b8f9b5a93c9d8b42b75722700
Александр Смолин <smolll123@mail.ru>
2021-08-27 19:48:12 +0300

    temporary set queryset in Foreign keys as None

--- school/forms.py
+++ school/forms.py
@@ -17,9 +17,9 @@ class TaskCreateForm(forms.ModelForm):
     task_name = forms.CharField(max_length=100, required=True)
     start_date = forms.DateField(required=True)
     end_date = forms.DateField(required=True)
-    class_id = forms.ModelChoiceField(required=True)
-    teacher_id = forms.ModelChoiceField(required=True)
-    discipline_id = forms.ModelChoiceField(required=True)
+    class_id = forms.ModelChoiceField(required=True, queryset=None)
+    teacher_id = forms.ModelChoiceField(required=True, queryset=None)
+    discipline_id = forms.ModelChoiceField(required=True, queryset=None)
     commentary = forms.CharField(required=True, widget=forms.Textarea(attrs={
         "rows": 10,
         "cols": 10,
