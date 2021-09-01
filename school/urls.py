9f679cceb961c17fc65d4e868139d90959a63c4f
Александр Смолин <smolll123@mail.ru>
2021-09-01 23:05:54 +0300

    fixing eeror: need to add pk

--- school/urls.py
+++ school/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('manager/students/', views.StudentListView.as_view(), name="student_list"),
     path('manager/teachers/', views.TeacherListView.as_view(), name="teacher_list"),
     path('manager/discipline/create/', views.DisciplineCreateView.as_view(), name="discipline_create"),
-    path('manager/discipline/update/', views.DisciplineUpdateView.as_view(), name="discipline_update"),
+    path('manager/discipline/<int:pk>/update/', views.DisciplineUpdateView.as_view(), name="discipline_update"),
 ]
