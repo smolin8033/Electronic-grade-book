58b6899b9433f547829ee9336af25c152f7ec3b2
Александр Смолин <smolll123@mail.ru>
2021-09-09 18:55:21 +0300

    add get_absolute_url method to class Student

--- school/models.py
+++ school/models.py
@@ -39,6 +39,9 @@ class Student(models.Model):
     def get_full_name(self):
         return "%s %s %s" % (self.first_name, self.second_name, self.family_name)
 
+    def get_absolute_url(self):
+        return reverse("manager_class", kwargs={"pk": self.class_id.id})
+
     def __str__(self):
         return "%s %s %s, address: %s " % \
                (self.first_name, self.second_name, self.family_name, self.address)
