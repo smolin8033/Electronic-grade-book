6da87c3dc3c76d20bf27f9f7002ad0ff8d386218
Александр Смолин <smolll123@mail.ru>
2021-09-14 12:50:20 +0300

    refactor

--- school/urls.py
+++ school/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_view, name='test_view'),
+    path('', views.login_view, name='test_view'),
     path('student/current/', views.student_current, name='student_current'),
     path('student/all/', views.student_all, name='student_all'),
     path('teacher/', views.teacher_interface, name='teacher_interface'),
