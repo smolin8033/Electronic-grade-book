dffa03e4fd57b47da5b1e550a2cda708d072eea7
Александр Смолин <smolll123@mail.ru>
2021-09-18 19:12:02 +0300

    delete <int:pk>

--- school/urls.py
+++ school/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
-    path('student/<int:pk>/current/', views.student_current, name='student_current'),
+    path('student/current/', views.student_current, name='student_current'),
     path('student/all/', views.student_all, name='student_all'),
     path('teacher/', views.teacher_interface, name='teacher_interface'),
     path('teacher/<int:pk>/class/', views.class_students, name="class_students"),
